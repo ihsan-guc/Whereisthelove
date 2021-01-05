72644a014a94f2433c5868824b76ef3ef5b57805
ihsan-guc <ihsanguc.33@gmail.com>
2021-01-05 22:23:23 +0300

    .

--- Admin/Startup.cs
+++ Admin/Startup.cs
@@ -1,15 +1,11 @@
 ﻿using Admin.Core;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Whereisthelove.Data.Dal;
 using Whereisthelove.Data.Dal.Abstract;
 using Whereisthelove.Data.Dal.Concrete;
